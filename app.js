8b327b6a1ff46bdf5e891877dc81668ef80dde66
Amy Roche <amy.roche31@gmail.com>
2017-04-20 16:11:39 -0600

    4 cats

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-var petCount = document.getElementById("petCount")
+/*var petCount = document.getElementById("petCount")
 var catStatus = document.getElementById("catStatus")
 var catButton = document.getElementById("catButton")
-var catPic = document.getElementById("catPic")
+var catPic = document.getElementById("catPic")*/
 
-var mrFluffy = {
+/*var mrFluffy = {
+    id: 247,
     name: "Mr. Fluffy",
     imgUrl: "http://www.findcatnames.com/wp-content/uploads/2017/01/tabby-cat-names.jpg",
     happyStatus: "Purrr",
@@ -13,27 +14,119 @@ var mrFluffy = {
     petCount: 0
 };
 
-function petCat() {
-    mrFluffy.petCount++;
-    petCount.innerHTML = mrFluffy.petCount;
-    if(mrFluffy.petCount > 5) {
-        catStatus.innerHTML = mrFluffy.grumpyStatus;
+var whiskers = {
+    id: 1,
+    name: "Whiskers",
+    imgUrl: "http://www.lifewithcats.tv/wp-content/uploads/2012/10/cat-linncurrie.jpg",
+    happyStatus: "Meow?",
+    grumpyStatus: "Grrrr",
+    feralStatus: "CLAWS!",
+    goneStatus: "He packed his bags and ran off",
+    petCount: 0
+};*/
+
+var cats = [{
+    id: 1,
+    name: "Whiskers",
+    imgUrl: "http://www.lifewithcats.tv/wp-content/uploads/2012/10/cat-linncurrie.jpg",
+    happyStatus: "Meow?",
+    grumpyStatus: "Grrrr",
+    feralStatus: "CLAWS!",
+    goneStatus: "He packed his bags and ran off",
+    petCount: 0
+}, {
+    id: 2,
+    name: "Mr. Fluffy",
+    imgUrl: "http://www.findcatnames.com/wp-content/uploads/2017/01/tabby-cat-names.jpg",
+    happyStatus: "Purrr",
+    grumpyStatus: "Rawwwwwr",
+    feralStatus: "Hissss",
+    goneStatus: "He time traveled away",
+    petCount: 0
+},
+{
+    id: 3,
+    name: "Dog",
+    imgUrl: "http://www.petmd.com/sites/default/files/what-does-it-mean-when-cat-wags-tail.jpg",
+    happyStatus: "Mrowww",
+    grumpyStatus: "Hiss Hiss",
+    feralStatus: "Grr Hisk hisk",
+    goneStatus: "Cat runs off and climbs a tree",
+    petCount: 0
+},{
+    id: 4,
+    name: "Tubby",
+    imgUrl: "http://media.mnn.com/assets/images/2016/02/angry-cat.jpg.838x0_q80.jpg",
+    happyStatus: "*Silence*",
+    grumpyStatus: "*walks away*",
+    feralStatus: "*plots world domination*",
+    goneStatus: "Where did the cat go?",
+    petCount: 0
+}];
+
+function petCat(catId) {
+    var petCount = document.getElementById("petCount"+catId)
+    var catStatus = document.getElementById("catStatus"+catId)
+    var catButton = document.getElementById("catButton"+catId)
+    var catPic = document.getElementById("catPic"+catId)
+
+    var targetCat = getCatById(catId);
+
+
+    targetCat.petCount++;
+    petCount.innerHTML = targetCat.petCount;
+    if(targetCat.petCount > 4) {
+        catStatus.innerHTML = targetCat.grumpyStatus;
     }
-    if (mrFluffy.petCount == 9) {
-        catStatus.innerHTML = mrFluffy.feralStatus;
+    if (targetCat.petCount == 9) {
+        catStatus.innerHTML = targetCat.feralStatus;
     }
-    if (mrFluffy.petCount == 10) {
-        catStatus.innerHTML = mrFluffy.goneStatus;
+    if (targetCat.petCount == 10) {
+        catStatus.innerHTML = targetCat.goneStatus;
         catButton.disabled = true;
         catPic.src = "";
         petCount.innerHTML = "";
     }
 }
 
-function comeBack() {
-    mrFluffy.petCount++;
-    petCount.innerHTML = mrFluffy.petCount;
-    catButton.disabled = false;
-    catStatus.innerHTML = mrFluffy.happyStatus;
-    catPic.src = mrFluffy.imgUrl;
+function getCatById(catId){
+    for (var i = 0; i < cats.length; i++){
+        var cat = cats[i];
+
+        if(cat.id == catId){
+            return cat;
+        }
+    }
 }
+
+function comeBack(catId) {
+    var petCount = document.getElementById("petCount"+catId)
+    var catStatus = document.getElementById("catStatus"+catId)
+    var catButton = document.getElementById("catButton"+catId)
+    var catPic = document.getElementById("catPic"+catId)
+    var targetCat = getCatById(catId);
+    targetCat.petCount = 0;
+    petCount.innerHTML = targetCat.petCount;
+    catButton.disabled = false;
+    catStatus.innerHTML = targetCat.happyStatus;
+    catPic.src = targetCat.imgUrl;
+}
+
+function drawCats(listOfCats){
+    var template = "";
+    for (var i = 0; i < listOfCats.length; i++){
+        var cat = listOfCats[i];
+        template += `
+        <div>
+        <img id="catPic${cat.id}" src="${cat.imgUrl}">
+        <br><button type="button" id="catButton${cat.id}" onClick="petCat(${cat.id})">Pet</button>
+        <button type-"button" id="comeBack${cat.id}" onClick="comeBack(${cat.id})">Come back Kitty</button>
+        <h1 id="petCount${cat.id}">${cat.petCount}</h1>
+        <h2 id="catStatus${cat.id}">${cat.happyStatus}</h2>
+        </div>
+        `
+    }
+document.getElementById("cat-list").innerHTML = template;
+}
+
+drawCats(cats);
