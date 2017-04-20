dbdc145a88433bab8da8966e2458c5e1f7de801f
Amy Roche <amy.roche31@gmail.com>
2017-04-20 14:12:41 -0600

    new cat

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 var petCount = document.getElementById("petCount")
 var catStatus = document.getElementById("catStatus")
-var counter = 0;
 var catButton = document.getElementById("catButton")
 var catPic = document.getElementById("catPic")
 
+var mrFluffy = {
+    name: "Mr. Fluffy",
+    imgUrl: "http://www.findcatnames.com/wp-content/uploads/2017/01/tabby-cat-names.jpg",
+    happyStatus: "Purrr",
+    grumpyStatus: "Rawwwwwr",
+    feralStatus: "Hissss",
+    goneStatus: "He time traveled away",
+    petCount: 0
+};
+
 function petCat() {
-    counter++;
-    petCount.innerHTML = counter;
-    if(counter > 5) {
-        catStatus.innerHTML = "RAWR!!!"
+    mrFluffy.petCount++;
+    petCount.innerHTML = mrFluffy.petCount;
+    if(mrFluffy.petCount > 5) {
+        catStatus.innerHTML = mrFluffy.grumpyStatus;
     }
-    if (counter == 9) {
-        catStatus.innerHTML = "HISSSSSS!!!"
+    if (mrFluffy.petCount == 9) {
+        catStatus.innerHTML = mrFluffy.feralStatus;
     }
-    if (counter == 10) {
-        catStatus.innerHTML = "Cat ran away!";
+    if (mrFluffy.petCount == 10) {
+        catStatus.innerHTML = mrFluffy.goneStatus;
         catButton.disabled = true;
         catPic.src = "";
         petCount.innerHTML = "";
@@ -22,9 +31,9 @@ function petCat() {
 }
 
 function comeBack() {
-    counter = 0;
-    petCount.innerHTML = counter;
+    mrFluffy.petCount++;
+    petCount.innerHTML = mrFluffy.petCount;
     catButton.disabled = false;
-    catStatus.innerHTML = "Purrrr"
-    catPic.src = "http://www.findcatnames.com/wp-content/uploads/2017/01/tabby-cat-names.jpg"
+    catStatus.innerHTML = mrFluffy.happyStatus;
+    catPic.src = mrFluffy.imgUrl;
 }
